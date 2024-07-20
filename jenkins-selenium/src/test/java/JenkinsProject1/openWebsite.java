252bac1df0c856c2e7380ba4171ed403ab363942
dvenkatramana1998 <dhammalapativenkataramana570@gmail.com>
2024-07-20 19:51:57 +0530

    projectsetup

---
+++ jenkins-selenium/src/test/java/JenkinsProject1/openWebsite.java
@@ -0,0 +1,20 @@
+package JenkinsProject1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class openWebsite {
+	
+	@Test
+	public void website() {
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.name("email")).sendKeys("ssssssssssssssssss");
+		driver.findElement(By.name("pass")).sendKeys("rrrrrrrrrrrrrrrrrrrrrrrrr");
+		driver.findElement(By.name("login")).click();
+		
+	}
+
+}
